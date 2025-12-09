1b8f2067dfc6083ece758a827f3ce56b0812902f
dhanalaxmi <dhanalaxmidupakuntla@gmail.com>
2025-12-09 13:53:08 +0530

    TImeflow

--- module-3/project/src/footer.js
+++
@@ -1,6 +0,0 @@
-export function footer(){
-    return `
-    <footer style="margin-top: 20px;background: #222; color: #fff; text-align: center;">
-        @2025 Masai Learn to Code
-    </footer>`
-}
--- module-3/project/src/main.js
+++
@@ -1,5 +0,0 @@
-import { navbar } from "../component/navbar";
-import { footer } from "../src/footer";
-
-document.getElementById("nav").innerHTML = navbar();
-document.getElementById("footsection").body.innerHTML = footer();
